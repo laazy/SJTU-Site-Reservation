ccf23be9231cbac1bb203c9aae9b03b227fbc560
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2017-12-22 20:09:53 +0800

    done the calender oopeation

---
+++ src/components/content/sport.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+    import Search from "./search"
+    export default {
+        name: "sport",
+        component : {
+          Search
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/content/study.vue
@@ -0,0 +1,17 @@
+<template>
+
+</template>
+
+<script>
+    import Search from "./search"
+    export default {
+        name: "study",
+      component : {
+          Search
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
