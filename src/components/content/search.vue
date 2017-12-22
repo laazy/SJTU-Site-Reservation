50382cef9d22b83c87ce6b3bfc68537ec0d62bf6
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2017-12-22 20:55:22 +0800

    serach box complete!

--- src/components/content/search.vue
+++ src/components/content/search.vue
@@ -1,12 +1,9 @@
 <template>
     <div>
       <select v-model="hall_type">
-        <option value="hash table">1222</option>
-        <option v-for="(key,value) in room_list" v-bind:value="key">{{key}}</option>
+        <option v-for="(value,key) in room_list" v-bind:value="key">{{key}}</option>
       </select>
-      <input type = "date" v-model="time" >
-      <p>{{hall_type}}</p>
-      <p v-for="(key,value) in room_list">{{key}}  {{value}}</p>
+      <input type = "date" v-model="time">
     </div>
 </template>
 
@@ -14,10 +11,9 @@
     import rooms from "../../assets/room_list.json"
     export default {
         name: "search",
-        created() {
+        mounted() {
             this.initTime();
             this.initRoomlist();
-            console.log(rooms);
             //this.data().room_list = this.data().site_type;
         },
         data() {
@@ -25,7 +21,8 @@
                 time: "",
                 site_type:"study_site",
                 hall_type:"main_library",
-                room_list : rooms
+                //hall_type: this.site_type,
+                room_list:rooms["study_site"]  //[this.hall_type]
             }
         },
         methods : {
@@ -34,7 +31,8 @@
                 this.time = tmp.toDateString();
             },
             initRoomlist(){
-              this.room_list = rooms[this.site_type][this.hall_type];
+              this.room_list = rooms[this.site_type][this.hall_type]
+              //this.site_type = " "
             }
         }
     }
