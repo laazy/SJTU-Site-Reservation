55cbba883ead464dd9a9ab299063ee160cc2d121
DESKTOP-AE3A1NF\61640 <laaazy@sjtu.edu.cn>
2017-12-22 21:33:55 +0800

    no message

--- src/components/content/content.vue
+++ src/components/content/content.vue
@@ -1,19 +1,31 @@
 <template>
-
+  <div class="btn-group btn-group-justified" role="group" aria-label="...">
+    <div class="btn-group" role="group" id="study">
+      <button type="button" class="btn btn-default" v-on:click="set(true)">学习场馆</button>
+    </div>
+    <div class="btn-group" role="group">
+      <button type="button" class="btn btn-default" v-on:click="set(false)">运动场馆</button>
+    </div>
+  </div>
 </template>
 
 <script>
-    import Sport from "./sport"
-    import Study from "./study"
-    export default{
-        name: "content",
-        component :{
-          Sport,
-          Study
-        }
-    }
+
+
+  export default {
+    el: '#study',
+
+    method:{
+      function set(value) {
+    studyshow = value
+  }
+  }
+  }
+
+
 </script>
 
-<style scoped>
+<style>
 
 </style>
+
