e8e3908a2ed8f052c968027ceba82b83ea422f71
laazy616408874@qq.com <laaazy@sjtu.edu.cn>
2017-12-23 21:47:05 +0800

    完善了content的结构，并组织了其他组件的父子关系

--- src/components/content/content.vue
+++ src/components/content/content.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="content">
+    <introduction v-show="intro_show"></introduction>
     <div class="btn-group btn-group-justified" role="group" aria-label="...">
       <div class="btn-group" role="group" id="study">
         <button type="button" class="btn btn-default" v-on:click="setStudy">学习场馆</button>
@@ -8,32 +9,38 @@
         <button type="button" class="btn btn-default" v-on:click="setSport">运动场馆</button>
       </div>
     </div>
-    <study v-show="study_show">ada</study>
-    <sport v-show="sport_show">dsd</sport>
+    <study v-show="study_show"></study>
+    <sport v-show="sport_show"></sport>
   </div>
 </template>
 
 <script>
   import Study from "./study.vue";
   import Sport from "./sport.vue";
+  import Introduction from "../Introduction/Introduction"
+
   export default{
     name: "Content",
     data(){
       return{
         study_show:false,
-        sport_show:false
+        sport_show:false,
+        intro_show:true
       }
     },
     components:{
       Study,
-      Sport
+      Sport,
+      Introduction
     },
     methods:{
       setStudy:function(event){
+        this.intro_show = false;
         this.study_show = true;
         this.sport_show = false;
       },
       setSport:function(event){
+        this.intro_show = false;
         this.study_show = false;
         this.sport_show = true;
       }
