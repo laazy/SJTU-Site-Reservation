86ddc9e1808382e4e1d963032c31db7b8157491f
laazy616408874@qq.com <laaazy@sjtu.edu.cn>
2017-12-25 00:09:39 +0800

    完成了study的选定、提交、效验功能

--- src/components/content/study.vue
+++ src/components/content/study.vue
@@ -1,41 +1,99 @@
 ﻿<template>
 <div id="study">
   <img src="../../Image/学习.png" height="300" width="300"/>
-    <search ref="search" v-on:pass_rosta="getRoomStates"></search>
-    <dem class="demo">
-      <div id="seat-map" class="seatCharts-container" tabindex="0" >
+  <search ref="search" v-on:pass_rosta="getRoomStates"></search>
+  <div class="demo">
+    <div id="seat-map" class="seatCharts-container" tabindex="0" >
       <div v-for="(value,key) in room_states " >
-      <div class="seatCharts-row"></div>
-      <div class="seatCharts-cell seatCharts-space">{{key}}</div>
-      <div class="seatCharts-cell seatCharts-space"></div>
-      <div v-for="valuex in value" >
-        <div v-if = "valuex == 0" id="2_1" role="checkbox" aria-checked="false" focusable="true" tabindex="-1" class="seatCharts-seat seatCharts-cell available" ></div>
-        <div v-else ="valuex == 1" id="2_2" role="checkbox" aria-checked="false" focusable="true" tabindex="-1" class="seatCharts-seat seatCharts-cell unavailable"></div>
+        <div class="seatCharts-row"></div>
+        <div class="seatCharts-cell seatCharts-space">{{key}}</div>
+        <div class="seatCharts-cell seatCharts-space"></div>
+        <div v-for="(valuex,index) in value" >
+          <div v-if = "valuex == 0" @click="selectTime(key,index)" id="2_0" role="checkbox" aria-checked="false" focusable="true" tabindex="-1" class="seatCharts-seat seatCharts-cell available" ></div>
+          <div v-else-if ="valuex == 2" @click="cancelTime(key,index)" id="2_2" role="checkbox" aria-checked="false" focusable="true" tabindex="-1" class="seatCharts-seat seatCharts-cell selected"></div>
+          <div v-else-if ="valuex == 3"  id="2_3" role="checkbox" aria-checked="false" focusable="true" tabindex="-1" class="seatCharts-seat seatCharts-cell auditting"></div>
+          <div v-else="values == 1" id="2_1" role="checkbox" aria-checked="false" focusable="true" tabindex="-1" class="seatCharts-seat seatCharts-cell unavailable"></div>
+        </div>
       </div>
-
-   </div>
-
+    </div>
   </div>
-</dem>
+  <button id="submit_button" @click="submitOrder">确认提交</button>
 </div>
 </template>
 
 <script>
+    var MAX_AMOUNT_TIME = 8
     import Search from "./search.vue"
     export default {
       name: "study",
       data(){
         return{
-          room_states:{}
+          room_states:{},
+          select_room:"",
+          select_amount:0
         }
       },
       methods: {
         getRoomStates:function(room_status){
-          console.log("have get the data",room_status);
+          //console.log("have get the data",room_status);
           this.room_states = room_status;
         },
         init(){
           this.$refs.search.initStudy()
+        },
+        selectTime(key,index){
+          if(this.select_room == "" || this.select_amount == 0)
+            this.select_room = key;
+          if (this.select_room != key )
+          {
+            alert("you can only order exact one room once!")
+            return;
+          }
+          if (this.select_amount >= MAX_AMOUNT_TIME)
+          {
+            var hours = MAX_AMOUNT_TIME/2;
+            alert("you can only order "+hours.toString()+" hours once")
+            return
+          }
+          this.select_amount ++
+          this.$set(this.room_states[key],index,2);
+          this.$set(this.room_states,key,this.room_states[key]);
+        },
+        cancelTime(key,index){
+          this.select_amount --
+          this.$set(this.room_states[key],index,0);
+          this.$set(this.room_states,key,this.room_states[key]);
+          //console.log("cancel",this.room_states[key][index])
+        },
+        submitOrder(){
+          if (!this.ifValid())
+          {
+            alert("you must order the continuous time")
+            return
+          }
+          var array = this.room_states[this.select_room]
+          var len = array.length
+          for (var i = 0; i< len;i++)
+            if (array[i] == 2)
+              this.$set(this.room_states[this.select_room],i,3)
+          this.$set(this.room_states,this.select_room,array);
+          this.select_amount = 0
+          this.select_room = ""
+        },
+        ifValid(){
+          var rooms = this.room_states[this.select_room]
+          var meet_o = false
+          var meet_2 = false
+          var cont = true
+          for (var i in rooms){
+            if (i == 2)
+              meet_2 = true
+            if (meet_2 && i != 2)
+              meet_o = true
+            if (meet_o && i == 2)
+              cont = false
+          }
+          return cont
         }
       },
       components : {
@@ -78,20 +136,20 @@
       height: 450px;
     }
 
-      .booking-details h3 {
+    .booking-details h3 {
         margin: 5px 5px 0 0;
         font-size: 16px;
-      }
+    }
 
-      .booking-details p {
+    .booking-details p {
         line-height: 26px;
         font-size: 16px;
         color: #999
-      }
+    }
 
-        .booking-details p span {
+    .booking-details p span {
           color: #666
-        }
+    }
 
     div.seatCharts-cell {
       color: #182C4E;
@@ -118,16 +176,15 @@
     }
 
     div.seatCharts-seat.available {
-      background-color: #B9DEA0;
+      background-color: #58de4b;
     }
 
-    div.seatCharts-seat.focused {
-      background-color: #76B474;
-      border: none;
+    div.seatCharts-seat.auditting {
+      background-color: #1a29b4;
     }
 
     div.seatCharts-seat.selected {
-      background-color: #E6CAC4;
+      background-color: #e6e447;
     }
 
     div.seatCharts-seat.unavailable {
